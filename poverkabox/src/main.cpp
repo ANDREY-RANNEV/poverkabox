0afabeda75a27762472be786d46f44328d92889f
Andrey Rannev <rannev@fguzsamo.ru>
2023-06-02 00:56:11 +0400

    add pio ini

--- poverkabox/src/main.cpp
+++ poverkabox/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <U8g2lib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+#define I2C_SDA PB7
+#define I2C_SCL PB6
+
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+u8g2.begin(); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+   u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+    u8g2.drawStr(0,24,"Hello World!");
+  } while ( u8g2.nextPage() );
 }
