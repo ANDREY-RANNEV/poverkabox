4af458d7eaaee5b5cccfc22e9457ce88a7a3cd96
ANDREY-RANNEV <66748147+ANDREY-RANNEV@users.noreply.github.com>
2023-06-05 14:44:18 +0400

    111

--- poverkabox/src/main.cpp
+++ poverkabox/src/main.cpp
@@ -11,10 +11,13 @@
 // const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 // LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void setup() {
-    pinMode(PC13,OUTPUT);
+void setup()
+{
+    pinMode(PC13, OUTPUT);
 }
 
-void loop() {
-
+void loop()
+{
+    delay(500);
+    digitalWrite(PC13,digitalRead(PC13));
 }
