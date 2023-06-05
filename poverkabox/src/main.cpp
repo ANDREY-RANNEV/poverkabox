214be24ec9c531e097bb98f6a5e917e6c78e35f2
ANDREY-RANNEV <66748147+ANDREY-RANNEV@users.noreply.github.com>
2023-06-05 14:29:45 +0400

    fix

--- poverkabox/src/main.cpp
+++ poverkabox/src/main.cpp
@@ -1,23 +1,20 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <U8g2lib.h>
+#include <LiquidCrystal.h>
+// #include <Wire.h>
+// #include <U8g2lib.h>
 
-U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+// U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-#define I2C_SDA PB7
-#define I2C_SCL PB6
+// #define I2C_SDA PB7
+// #define I2C_SCL PB6
 
-
+// const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  // put your setup code here, to run once:
-u8g2.begin(); 
+    pinMode(PC13,OUTPUT);
 }
 
 void loop() {
-   u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.drawStr(0,24,"Hello World!");
-  } while ( u8g2.nextPage() );
+
 }
