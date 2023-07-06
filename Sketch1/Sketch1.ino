a6a952ecb45856d475c8a9ca4783bdb08e7df5a5
Andrey Rannev <rannev@fguzsamo.ru>
2023-07-07 02:21:02 +0400

    add

---
+++ Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	07.07.2023 2:13:24
+ Author:	Rannev
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
