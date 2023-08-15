22560794258983a5a2c1ca7943d57057e821fa2f
Andrey Rannev <rannev@fguzsamo.ru>
2023-08-15 08:17:50 +0400

    fix redmi 9a

--- POVERKA_APP/MainActivity.cs
+++ POVERKA_APP/MainActivity.cs
@@ -7,6 +7,8 @@ using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
+using ApxLabs.FastAndroidCamera;
+using Android.Hardware;
 
 namespace POVERKA_APP
 {
@@ -15,6 +17,35 @@ namespace POVERKA_APP
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            //Camera camera = Camera.Open();
+            //Camera.Parameters parameters = camera.GetParameters();
+
+            //// snip - set resolution, frame rate, preview format, etc.
+
+            //camera.SetParameters(parameters);
+
+            //// assuming the SurfaceView has been set up elsewhere
+            //camera.SetPreviewDisplay(_surfaceView.Holder);
+            //camera.StartPreview();
+
+            //int numBytes = (parameters.PreviewSize.Width * parameters.PreviewSize.Height * ImageFormat.GetBitsPerPixel(parameters.PreviewFormat)) / 8;
+            //for (uint i = 0; i < NUM_PREVIEW_BUFFERS; ++i)
+            //{
+            //    using (FastJavaByteArray buffer = new FastJavaByteArray(numBytes))
+            //    {
+            //        // allocate new Java byte arrays for Android to use for preview frames
+            //        camera.AddCallbackBuffer(new FastJavaByteArray(numBytes));
+            //    }
+            //    // The using block automatically calls Dispose() on the buffer, which is safe
+            //    // because it does not automaticaly destroy the Java byte array. It only releases
+            //    // our JNI reference to that array; the Android Camera (in Java land) still
+            //    // has its own reference to the array.
+            //}
+
+            //// non-marshaling version of the preview callback
+            //camera.SetNonMarshalingPreviewCallback(this);
+
+           
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
