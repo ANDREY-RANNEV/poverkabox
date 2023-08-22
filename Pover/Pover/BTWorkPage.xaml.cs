da290c99c4228dcef1ff89742fc7db412f331ba5
Andrey Rannev <rannev@fguzsamo.ru>
2023-08-23 02:29:53 +0400

    add connect bt

--- Pover/Pover/BTWorkPage.xaml.cs
+++ Pover/Pover/BTWorkPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.BluetoothClassic.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,45 @@ namespace Pover
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BTWorkPage : ContentPage
     {
+        IBluetoothConnection Connection;
+        BluetoothDeviceModel BluetoothDevice;
+        IBluetoothAdapter BluetoothAdapter;
+
         public BTWorkPage()
         {
             InitializeComponent();
+            BluetoothAdapter = DependencyService.Resolve<IBluetoothAdapter>();
+            BluetoothDevice = (BluetoothDeviceModel)BindingContext;
+            if (BluetoothDevice != null)
+            {
+                Connection = BluetoothAdapter.CreateConnection(BluetoothDevice);
+                Connection.ConnectAsync().Wait();
+            }
+        }
+
+        private async void connectbt_Clicked(object sender, EventArgs e)
+        {
+            var device=(BluetoothDeviceModel)BindingContext;
+            if (device != null)
+            {
+                var adapter = DependencyService.Resolve<IBluetoothAdapter>();
+                using (var connection=adapter.CreateConnection(device))
+                {
+                    if(await connection.RetryConnectAsync(retriesCount:2))
+                    {
+                        byte[] buffer = Encoding.ASCII.GetBytes("{\"start\":1}");
+                        if(!(await connection.RetryTransmitAsync(buffer,0,buffer.Length)))
+                        {
+                            DisplayAlert("error", "cannot", "Close");
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("error", "cannot", "Close");
+                    }
+                }
+            }
+
         }
     }
 }
--- Pover/Pover/selectDevicePage.xaml.cs
+++ Pover/Pover/selectDevicePage.xaml.cs
@@ -23,15 +23,17 @@ namespace Pover
         {
             var adapter = DependencyService.Resolve<IBluetoothAdapter>();
             BoundedDevices.ItemsSource = adapter.BondedDevices;
+
         }
 
-        private async void BoundedDevices_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private  async void BoundedDevices_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             var device = (BluetoothDeviceModel)e.SelectedItem;
             if (device != null)
             {
-                await Navigation.PushAsync(new BTWorkPage { BindingContext=device})
+                await Navigation.PushAsync(new BTWorkPage() { BindingContext=device});
             }
+
             BoundedDevices.SelectedItem = null;
         }
     }
